19535d43aff2ce5abe392b7abdc053c82df98ff9
igorskyflyer <igor.dvlpr@gmail.com>
2020-11-25 23:22:20 +0100

    Delegate features to RegKeys package

--- index.js
+++ index.js
@@ -1,65 +1,22 @@
-const spawnSync = require('child_process').spawnSync;
+const RegKeys = require('@igor.dvlpr/regkeys')
 
-const regKey = 'HKLM\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\App Paths';
-const searchKey =
-	'hkey_local_machine\\\\software\\\\microsoft\\\\windows\\\\currentversion\\\\app paths\\\\';
-const pattern = new RegExp(searchKey, 'g');
+const appPathKey = 'HKLM/SOFTWARE/Microsoft/Windows/CurrentVersion/App Paths'
+const registry = new RegKeys(appPathKey)
 
-let apps = [];
-
-function get() {
-	if (apps.length > 0) {
-		return apps;
-	}
-
-	try {
-		const shell = spawnSync('reg', ['query', regKey], {
-			stdio: 'pipe'
-		});
-		let output = shell.stdout.toString();
-
-		output = output.toLowerCase().replace(pattern, '');
-		apps = output.split('\r\n');
-
-		if (apps.length > 1) {
-			apps.shift();
-			apps.pop();
-		}
-	} catch (exp) {
-		console.error(exp);
-		return apps;
-	}
-
-	return apps;
+function get(forceRefresh = false) {
+  return registry.get(forceRefresh)
 }
 
-function has(list) {
-	const count = list.length;
-
-	if (!list || !list instanceof Array || count === 0) {
-		return apps;
-	}
-
-	const result = [];
-
-	if (apps.length === 0) {
-		apps = get();
-	}
-
-	for (let i = 0; i < count; i++) {
-		const matchFound = apps.indexOf(list[i]) > -1;
-		result.push(matchFound);
-	}
-
-	return result;
+function has(keys) {
+  return registry.hasKeys(keys)
 }
 
 function refresh() {
-	apps = [];
+  registry.clear()
 }
 
 module.exports = {
-	get,
-	has,
-	refresh
-};
+  get,
+  has,
+  refresh,
+}
