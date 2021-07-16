ede3650739de64d8e64a0b0361ae681af3601beb
igorskyflyer <igor.dvlpr@gmail.com>
2021-07-16 20:26:27 +0200

    Add JSDocs
    fixes #5

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 const RegKeys = require('@igor.dvlpr/regkeys')
 
-const appPathKey = 'HKLM/SOFTWARE/Microsoft/Windows/CurrentVersion/App Paths'
-const registry = new RegKeys(appPathKey)
+const appPathsKey = 'HKLM/SOFTWARE/Microsoft/Windows/CurrentVersion/App Paths'
+const registry = new RegKeys(appPathsKey)
 
+/**
+ * Synchronously gets AppPaths.
+ *
+ * NOTE: Results are **cached** internally!
+ * @param {boolean} [forceRefresh=false]
+ * @throws Throws an error if the host machine is not running Windows OS.
+ * @returns {string[]}
+ */
 function get(forceRefresh = false) {
   return registry.get(forceRefresh)
 }
 
-function has(keys) {
-  return registry.hasKeys(keys)
+/**
+ * Synchronously checks whether the given keys are present in the AppPaths.
+ * @param {string[]} list
+ * @param {boolean} [caseSensitive=false]
+ * @returns {boolean[]}
+ */
+function has(keys, caseSensitive = false) {
+  return registry.hasKeys(keys, caseSensitive)
 }
 
+/**
+ * Clears the cached result, if any.
+ * @returns {void}
+ */
 function refresh() {
   registry.clear()
 }
